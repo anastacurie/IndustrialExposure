169dccde29d90fa8e9f2bebe53804ca9d6dd3d57
anastacurie <anastacurie@gmail.com>
2021-11-24 10:25:19 +0200

    First Commit 'login' Wendesday 24-11-2021

--- lib/login.dart
+++ lib/login.dart
@@ -41,19 +41,142 @@ class LoginRoute extends StatelessWidget {
                   ),
                 ),
                 Container(
-                    height: size.height * 0.1,
-                    width: size.width,
-                    margin: const EdgeInsets.only(right: 20),
-                    child: const Text(
-                      "Login",
-                      textAlign: TextAlign.right,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ))
+                  height: size.height * 0.1,
+                  width: size.width,
+                  margin: const EdgeInsets.only(right: 20),
+                  child: const Text(
+                    "Login",
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
               ],
             ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(25),
+            child: Column(
+              children: [
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 0),
+                          blurRadius: 5,
+                          spreadRadius: -3,
+                          color: Colors.black),
+                    ],
+                  ),
+                  margin: const EdgeInsets.only(top: 10, bottom: 15),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      isDense: true, // used to size(height) textField
+                      contentPadding:
+                          EdgeInsets.all(11), // used to size(height) textField
+                      enabledBorder: InputBorder.none,
+                      icon: Icon(
+                        Icons.mail,
+                      ),
+                      hintText: "Email",
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 0),
+                          blurRadius: 5,
+                          spreadRadius: -3),
+                    ],
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      isDense: true,
+                      contentPadding: EdgeInsets.all(11),
+                      enabledBorder: InputBorder.none,
+                      icon: Icon(Icons.password_sharp),
+                      hintText: "Password",
+                    ),
+                  ),
+                ),
+                Container(
+                  width: size.width,
+                  child: const Text(
+                    "Forgot Password?",
+                    textAlign: TextAlign.right,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    // print("You Have Clicked The Button");
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 100,
+                    ),
+                    width: size.width,
+                    height: 40,
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: [
+                          Colors.orange,
+                          Colors.deepOrange,
+                        ],
+                      ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "LOGIN",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                // Text("data"),
+                Center(
+                  //child: Text("data"),
+                  child: RichText(
+                    text: const TextSpan(
+                      text: "Don't have account? ",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: "Register",
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            width: size.width,
           ),
         ],
       ),
